4efa54e78951abcc0236bdf4be8ea08e617a2c83
Megan Martinson <minimart.geek@gmail.com>
2017-06-09 13:24:51 -0500

    adding ability to deploy testingDeploy branch

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -15,7 +15,7 @@
 # $DOCKER_NAMESPACE_DEV - the image name for development deployments [Default]: reactioncommerce/prequel
 
 
-if [[ "$CIRCLE_BRANCH" != "master" && "$CIRCLE_BRANCH" != "development" ]]; then
+if [[ "$CIRCLE_BRANCH" != "master" && "$CIRCLE_BRANCH" != "development"  && "$CIRCLE_BRANCH" != "testingDeploy"]]; then
   echo "Not running a deployment branch."
   exit 0
 fi
@@ -39,7 +39,7 @@ fi
 
 
 # Master branch deployment (only runs when a version git tag exists - syntax: "v1.2.3")
-if [[ "$CIRCLE_BRANCH" == "master" ]]; then
+if [[ "$CIRCLE_BRANCH" == "master" || "$CIRCLE_BRANCH" == "testingDeploy" ]]; then
   VERSION=$(git describe --tags | grep "/ee-v[0-9]+(\.[0-9]+)*/")
 
   if [[ "$VERSION" ]]; then
